ea29343c6bbcd145134d9067fc7d0b2d8d7b04ab
LuisSempre <luis.silva@viannasempre.com.br>
2023-01-22 15:48:42 -0300

    Install React, Eslint, Prettier, Types

--- .history/astro.config_20230122152604.mjs
+++ .history/astro.config_20230122152604.mjs
@@ -2,10 +2,7 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-import react from "@astrojs/react";
-
-// https://astro.build/config
 export default defineConfig({
   site: "https://stack-uses.com",
-  integrations: [sitemap(), react()]
+  integrations: [sitemap()],
 });
